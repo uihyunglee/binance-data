053802e6ea372c5a18a324ce023c9bd19b2282eb
uihyunglee <uihyunglee.tech@gmail.com>
2023-12-09 22:35:15 +0900

    FIX: Delete 1M in  VALID_INTERVALS
    - Postgresql does not distinguish between upper and lowercase letters. (1m == 1M)

--- updater_binance.py
+++ updater_binance.py
@@ -9,7 +9,7 @@ import pandas as pd
 import psycopg2
 from binance.client import Client
 
-VALID_INTERVALS = ['1m', '3m', '5m', '30m', '1h', '2h', '4h', '6h', '8h', '1d', '1w', '1M']
+VALID_INTERVALS = ['1m', '3m', '5m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '1w']
 DB_FORM_CHANGE_LEVEL = VALID_INTERVALS.index('1d')  # intraday form -> daily form
 
 
